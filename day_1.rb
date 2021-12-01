d0c50025acf4bd8c1924163007539a725c392d59
bernardkroes <bernard@shiftf5.nl>
2021-12-01 15:22:53 +0100

    shortened and added explanation, not sure about readability

--- day_1.rb
+++ day_1.rb
@@ -1,18 +1,12 @@
 all_entries = File.read('day_1_input.txt').split("\n").map(&:to_i)
 
 inc_count = 0
-all_entries[1..-1].each_with_index do |e,i|
-  if e > all_entries[i]
-    inc_count += 1
-  end
-end
-
+look_ahead = 1
+all_entries[0..(-1-look_ahead)].each_with_index { |e,i| inc_count += 1 if all_entries[i+look_ahead] > e }
 puts inc_count
 
+# no need to sum because the sliding windows are overlapping, we can igonre the overlap, and just need to compare the element with the element that is ahead (1 or 3 steps)
 inc_count = 0
-all_entries[0..-4].each_with_index do |e,i|
-  if all_entries[i+3] > e
-    inc_count += 1
-  end
-end
+look_ahead = 3
+all_entries[0..(-1-look_ahead)].each_with_index { |e,i| inc_count += 1 if all_entries[i+look_ahead] > e }
 puts inc_count
