16f830b742c9ba5fcae8bce0c53186ceca180858
bernardkroes <bernard@shiftf5.nl>
2021-12-02 15:00:44 +0100

    I like amount better than amt

--- day_2.rb
+++ day_2.rb
@@ -2,26 +2,26 @@ all_lines = File.read('day_2_input.txt').split("\n")
 
 hor_pos = depth = 0
 all_lines.each do |line|
-  cmd, amt_string = line.split(" ")
-  amt = amt_string.to_i
+  cmd, amount_string = line.split(" ")
+  amount = amount_string.to_i
 
-  hor_pos += amt if cmd == "forward"
-  depth += amt if cmd == "down"
-  depth -= amt if cmd == "up"
+  hor_pos += amount if cmd == "forward"
+  depth += amount if cmd == "down"
+  depth -= amount if cmd == "up"
 end
 puts hor_pos * depth
 
 hor_pos = depth = aim = 0
 all_lines.each do |line|
-  cmd, amt_string = line.split(" ")
-  amt = amt_string.to_i
+  cmd, amount_string = line.split(" ")
+  amount = amount_string.to_i
 
   if cmd == "forward"
-    hor_pos += amt
-    depth += (amt * aim)
+    hor_pos += amount
+    depth += (amount * aim)
   end
-  aim += amt if cmd == "down"
-  aim -= amt if cmd == "up"
+  aim += amount if cmd == "down"
+  aim -= amount if cmd == "up"
 end
 puts hor_pos * depth
 
