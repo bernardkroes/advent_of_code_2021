9b86501b28e2527e8026e5f0b0120cc354e87f31
bernardkroes <bernard@shiftf5.nl>
2021-12-05 09:02:41 +0100

    moved to hash instead of arrays

--- day_5.rb
+++ day_5.rb
@@ -1,24 +1,25 @@
 class Grid
-  SIZE = 1000
-
   def initialize(in_filename)
-    @map = []
+    @grid = {}
     @lines = []
     File.open(in_filename).each do |line|
       @lines << line.chomp.gsub(" -> ",",").split(',').map(&:to_i)
     end
-    SIZE.times do
-      @map << [0] * SIZE
-    end
+  end
+
+  def inc_value_at(x,y)
+    the_key = "#{x}_#{y}"
+    @grid[the_key] = 0 unless @grid.has_key?(the_key)
+    @grid[the_key] += 1
   end
 
   def set_line_counts(do_part2)
     @lines.each do |l|
       x1, y1, x2, y2 = l
       if x1 == x2
-        [y1,y2].min.upto([y1,y2].max) { |y| @map[y][x1] += 1 }
+        [y1,y2].min.upto([y1,y2].max) { |y| inc_value_at(x1, y) }
       elsif y1 == y2
-        [x1,x2].min.upto([x1,x2].max) { |x| @map[y1][x] += 1 }
+        [x1,x2].min.upto([x1,x2].max) { |x| inc_value_at(x, y1) }
       elsif do_part2 # part 2 : diagonal lines
         start_y, end_y = y1, y2
         start_x = x1
@@ -30,24 +31,14 @@ class Grid
           delta_x = x1 > x2 ? 1 : -1
         end
         (end_y - start_y + 1).times do |i|
-          @map[start_y + i][start_x + i * delta_x] += 1
+          inc_value_at(start_x + i * delta_x, start_y + i)
         end
       end
     end
   end
 
-  def show_map
-    SIZE.times do |l|
-      puts @map[l].inspect
-    end
-  end
-
   def find_score
-    two_or_more_count = 0
-    SIZE.times do |l|
-      two_or_more_count += @map[l].count { |e| e >= 2 }
-    end
-    two_or_more_count
+    @grid.values.count { |e| e >= 2 }
   end
 end
 
