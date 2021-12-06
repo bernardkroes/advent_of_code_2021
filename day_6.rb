6ee9267b40d560c02f6781daa49df99d7a2b5090
bernardkroes <bernard@shiftf5.nl>
2021-12-06 07:26:32 +0100

    day 6

---
+++ day_6.rb
@@ -0,0 +1,29 @@
+all_fish = File.read('day_6_input.txt').split(",").map(&:to_i)
+# all_fish = "3,4,3,1,2".split(",").map(&:to_i)
+
+80.times do |d|
+  new_fish_count = all_fish.count { |f| f == 0 }
+  all_fish.each_with_index do |f,i|
+    f -= 1
+    f = 6 if f < 0
+    all_fish[i] = f
+  end
+  all_fish += [8] * new_fish_count
+end
+puts all_fish.size
+
+# part 2: optimized method
+# read input again
+all_fish = File.read('day_6_input.txt').split(",").map(&:to_i)
+day_counts = []
+9.times do |c|
+  day_counts << all_fish.count { |f| f == c }
+end
+
+256.times do |d|
+ new_fish_count = day_counts[0]
+ day_counts = day_counts.rotate
+ day_counts[8] = new_fish_count
+ day_counts[6] += new_fish_count
+end
+puts day_counts.sum
