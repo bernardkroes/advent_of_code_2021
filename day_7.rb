9dfa2edb296aa30033f6345535a85bfbd7563b89
bernardkroes <bernard@shiftf5.nl>
2021-12-07 08:17:27 +0100

    added an alternative for part2

--- day_7.rb
+++ day_7.rb
@@ -6,7 +6,13 @@ min_fuel = -1
   total_fuel = 0
   crab_positions.each do |crab_pos|
     # total_fuel += (crab_pos - pos).abs        # part 1
-    total_fuel += (0..(crab_pos - pos).abs).sum # part 2
+
+    # part 2
+    # total_fuel += (0..(crab_pos - pos).abs).sum # originally used this
+
+#    or use the triangle formula for a little speedup:
+    the_dist = (crab_pos - pos).abs
+    total_fuel += the_dist * (the_dist + 1) / 2
   end
   if total_fuel < min_fuel || min_fuel == -1
     min_fuel = total_fuel
