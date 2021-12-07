bae6c31d060cfd0f6a5f1733ab9f0f20bacdab33
bernardkroes <bernard@shiftf5.nl>
2021-12-07 06:34:21 +0100

    day 7

---
+++ day_7.rb
@@ -0,0 +1,16 @@
+crab_positions = File.read('day_7_input.txt').split(",").map(&:to_i)
+# crab_positions = = 16,1,2,0,4,2,7,1,2,14
+
+min_fuel = -1
+0.upto(crab_positions.max) do |pos|
+  total_fuel = 0
+  crab_positions.each do |crab_pos|
+    # total_fuel += (crab_pos - pos).abs        # part 1
+    total_fuel += (0..(crab_pos - pos).abs).sum # part 2
+  end
+  if total_fuel < min_fuel || min_fuel == -1
+    min_fuel = total_fuel
+  end
+end
+puts min_fuel
+
