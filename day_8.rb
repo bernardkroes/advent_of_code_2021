5253d117c4205deb21757841109cdf264ed0c624
bernardkroes <bernard@shiftf5.nl>
2021-12-08 22:23:48 +0100

    added some comments

--- day_8.rb
+++ day_8.rb
@@ -30,25 +30,25 @@ all_lines.each do |line|
     values[i] = 8 if k.length == 7
     values[i] = 1 if k.length == 2
 
-    if k.length == 2 # dive in
+    if k.length == 2 # dive in: we can use the '1' leds to find the 3 and 6 by looking at the number of differing leds, same for 3 (-> 9) and 6 (-> 5)
       sorted_inputs.each_with_index do |other_k,j|
-        # subtracting 1 from 3 (length 5 only) leaves 3 on's
+        # subtracting 1 from 3 (for 5-led numbers) leaves 3 leds on
         if other_k.length == 5 && (other_k.chars - k.chars).length == 3
           values[j] = 3
-          sorted_inputs.each_with_index do |k3,jj| # subtracting 3 from 9 (length 6 only) leaves 1 on
+          sorted_inputs.each_with_index do |k3,jj| # subtracting 3 from 9 (for 6-led numbers only) leaves 1 led on
             values[jj] = 9 if k3.length == 6 && (k3.chars - other_k.chars).length == 1
           end
-        elsif other_k.length == 6 && (other_k.chars - k.chars).length == 5 # subtracting 1 from 6 (length 6 only) leaves 5 on's
+        elsif other_k.length == 6 && (other_k.chars - k.chars).length == 5 # subtracting 1 from 6 (for 6-led numbers only) leaves 5 leds on
           values[j] = 6
-          sorted_inputs.each_with_index do |k3,jj| # subtracting 5 from 6 (length 5 only) leaves 1 on
+          sorted_inputs.each_with_index do |k3,jj| # subtracting 5 from 6 (for 6-led numbers only) leaves 1 led on
             values[jj] = 5 if k3.length == 5 && (other_k.chars - k3.chars).length == 1
           end
         end
       end
     end
   end
-  # remaining of length 6 -> 0
-  # remaining of length 5 -> 2
+  # only remaining with 6-leds on -> 0
+  # only remaining with 5-leds on -> 2
   values.each_with_index do |v,i|
     if v == -1
       values[i] = 0 if sorted_inputs[i].length == 6
