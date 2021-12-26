0aa412306c29bd2ae784226336db80548113c303
bernardkroes <bernard@shiftf5.nl>
2021-12-26 09:07:27 +0100

    Get rid of the Marshalling

--- day_25.rb
+++ day_25.rb
@@ -30,15 +30,15 @@ class FloorMap
     @the_map[check_y][check_x] != "."
   end
 
-  def set_map(in_map, x, y, val)
+  def set_map(x, y, val)
     the_x, the_y = x, y
     the_y = 0 if y >= @size_y
     the_x = 0 if x >= @size_x
-    in_map[the_y][the_x] = val
+    @the_map[the_y][the_x] = val
   end
 
   def show_grid
-    puts "\e[H\e[2J" # clear the terminal for more fun
+    # puts "\e[H\e[2J" # clear the terminal for more fun
     @size_y.times do |y|
       puts @the_map[y]
     end
@@ -52,40 +52,44 @@ class FloorMap
   end
 
   def move_east
-    the_new_map = Marshal.load(Marshal.dump(@the_map))
     moved_count = 0
 
     @size_y.times do |y|
       @size_x.times do |x|
         if @the_map[y][x] == ">"
           if !is_occupied?(x + 1, y)
-            set_map(the_new_map, x + 1, y, ">")
-            set_map(the_new_map, x, y, ".")
+            set_map(x + 1, y, "E") # leave a T(emp) E(ast) trail: TE
+            set_map(x, y, "T")
             moved_count += 1
           end
         end
       end
     end
-    @the_map = Marshal.load(Marshal.dump(the_new_map))
+    @size_y.times do |y|
+      @the_map[y] = @the_map[y].gsub("E",">") # remove the trail
+      @the_map[y] = @the_map[y].gsub("T",".")
+    end
     moved_count
   end
 
   def move_south
-    the_new_map = Marshal.load(Marshal.dump(@the_map))
     moved_count = 0
 
     @size_y.times do |y|
       @size_x.times do |x|
         if @the_map[y][x] == "v"
           if !is_occupied?(x, y + 1)
-            set_map(the_new_map, x, y + 1, "v")
-            set_map(the_new_map, x, y, ".")
+            set_map(x, y + 1, "S")  # leave a T(emp) S(outh) trail: TS
+            set_map(x, y, "T")
             moved_count += 1
           end
         end
       end
     end
-    @the_map = Marshal.load(Marshal.dump(the_new_map))
+    @size_y.times do |y|
+      @the_map[y] = @the_map[y].gsub("S","v") # remove the trail
+      @the_map[y] = @the_map[y].gsub("T",".")
+    end
     moved_count
   end
 end
@@ -100,9 +104,8 @@ moved_count = 1
 while moved_count > 0
   moved_count = floormap.take_step
   the_step_count += 1
-  floormap.show_grid
+  # floormap.show_grid
 end
 
-floormap.show_grid
 puts the_step_count
 
